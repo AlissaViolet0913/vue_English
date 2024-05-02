b80132b285a3e094ac40b5d5345d0da9786609ed
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2024-05-02 13:44:02 +0900

    正誤機能完成

--- src/components/pages/Practice/[id].vue
+++ src/components/pages/Practice/[id].vue
@@ -4,8 +4,11 @@ import { ref, computed } from 'vue';
 import lesson from "@/assets/json/lesson.json"
 import { useRoute, useRouter } from 'vue-router';
 
+let isErrMsg = ref(false);
+let errMsg = ref("");
+let answer = ref("");
 let sentence = ref([]);;
-console.log(sentence.value)
+let nextBtn = ref(false);
 
 // クエリパラメータ(id)取得
 let route = useRoute();
@@ -32,71 +35,166 @@ const shuffleArray = (array) => {
 
 english = shuffleArray(english.value);
 
-// ボタンが押されたときにwordの値をsentenceに格納する
+// ボタンが押されたときにwordの値をsentenceに格納し、画面に表示されないようにする
 function addTosentence(word){
     sentence.value.push(word);
+    english = english.filter(item => item !== word);
+    console.log(english)
 }
+
+// englishとsentenceの配列要素が一致することを確認
+// 不一致が見つからなかったときのみtrueを返す
+function compareArrays(arr1, arr2) {
+    isErrMsg.value = false;
+
+    if (arr1.length !== arr2.length) {
+        isErrMsg.value = true;
+        errMsg.value = "すべての単語を使用してください";
+        answer.value = "";
+        return false;
+    } else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                isErrMsg.value = true;
+                errMsg.value = "不正解✖";
+                answer.value = "正解： " + lesson[n.value]?.english.join(" ") ;
+                nextBtn.value = true;
+                return false; // 不一致が見つかったらここで関数を終了してエラーメッセージを設定する
+            }
+        }
+        // 不一致が見つからなかった場合はエラーメッセージを設定せずにtrueを返す
+        return true;
+    }
+}
+
+
+
 
 // 「できた！」ボタン
 const router = useRouter();
 function levelUp(){
-    n.value += 1;
-    english = computed(() => shuffleArray(lesson[n.value]?.english));
-    sentence.value.push(english.value)
-    router.push(`/practice/${n.value + 1}`);
+    if(compareArrays(sentence.value, lesson[n.value]?.english)){
+        nextBtn.value = true;
+        isErrMsg.value = true;
+        errMsg.value = "正解◎";
+    };
     
     }
+
+// 「次の問題へ進む」ボタン
+function nextPage(){
+    n.value += 1;
+        english = computed(() => shuffleArray(lesson[n.value]?.english));
+        sentence.value.push(english.value)
+        sentence.value = [];
+        isErrMsg.value = false;
+        nextBtn.value = false;
+        router.push(`/practice/${n.value + 1}`);
+}
 </script>
 
 <template>
     <div class="practiceMain">
-        <p class="japanese">問題：「{{ japanese }}」
+        <p class="japanese">問題：<span class="japaneseSpn">「{{ japanese }}」</span>
         </p>
         <!-- join(""): 各要素を配列で区切られた文字列に連結 -->
         <p class="english">{{ sentence.join(' ') }}</p>
 
-        <div v-for="word in english" :key="word">
-            <button @click="addTosentence(word)">
-                {{ word }}
-            </button>
+        <div>
+            <p v-if="isErrMsg" class="errMsg1">{{ errMsg }}</p>
+            <p v-if="isErrMsg" class="errMsg2">{{ answer }}</p>
         </div>
-        <button @click="levelUp()" class="btn btn--blue btn--cubic btn--shadow" >
+
+        <div class="word-container">
+            <div v-for="word in english" :key="word" class="word-item">
+                <button @click="addTosentence(word)" class="word">
+                    {{ word }}
+                </button>
+            </div>
+        </div>
+
+        <button v-if="!nextBtn" @click="levelUp()" class="btn btn--blue btn--cubic btn--shadow" >
             できた！
+        </button>
+
+        <button v-if="nextBtn" @click="nextPage()" class="btn btn--blue btn--cubic btn--shadow">
+            次の問題へ進む
         </button>
     </div>
 </template>
 
 <style>
+
+.errMsg1{
+    color: red;
+    font-size: 4rem;
+    font-weight: bold;
+}
+
+.errMsg2{
+    color: red;
+    font-size: 5rem;
+    font-weight: bold;
+}
+
 .practiceMain{
     padding: 0%;
     margin-left: 20%;
     margin-right: 20%;
-    margin-top: 15%;
+    margin-top: 10%;
     text-align: center;
 }
 
 .japanese{
-    font-size: 30px;
+    font-size: 5rem;
+}
+
+.japaneseSpn{
+    color: rgb(112, 112, 112);
 }
 
 .english{
-    font-size: 25px;
+    font-size: 4rem;
     border-bottom: dotted;
     border-color: rgb(101, 142, 225);
     border-width: 4px;
+}
+
+.word{
+    background-color: rgb(219, 235, 249);
+    border: solid #aacffa;
+    border-radius: 4px;
+    font-size: 3rem;
+}
+
+.word:hover{
+    background-color: aliceblue;
+    border: solid #bbd8f9
+}
+
+.word-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 5%;
+}
+
+.word-item {
+    margin-right: 2%; 
+    margin-bottom: 1px; 
 }
 
 /* button */
 .btn{
     margin-top: 5%;
 }
+
 button.btn--blue {
   color: #fff;
   background-color: rgb(5, 68, 196);
   border-bottom: 5px solid rgb(5, 51, 144);
 }
 button.btn--blue:hover {
-  margin-top: 3px;
   color: #fff;
   background: rgb(34, 88, 197);
   border-bottom: 2px solid rgb(5, 51, 144);
